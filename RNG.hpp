f6f44d739bdb4fe591c5f308d129a64783d516c8
Jesse Windle <jesse.windle@gmail.com>
2012-11-04 20:18:35 -0600

    Realized I needed the GPL 3, thus made the necessary modifications.

--- RNG.hpp
+++ RNG.hpp
@@ -1,7 +1,7 @@
 // Copyright 2012 Jesse Windle - jwindle@ices.utexas.edu
 
 // This program is free software: you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License
+// modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation, either version 3 of
 // the License, or (at your option) any later version.
 
@@ -10,8 +10,8 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // General Public License for more details.
 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this program.  If not, see
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see
 // <http://www.gnu.org/licenses/>.
 
 //////////////////////////////////////////////////////////////////////
